6d7843873b49089ca682cef3a7dd5582ef555661
emin dev <110835380+arkodaz@users.noreply.github.com>
2022-08-27 16:15:48 +0400

    .

---
+++ cam/event.js
@@ -0,0 +1,11 @@
+AFRAME.registerComponent('markerhandler', {
+
+    init: function() {
+        const animatedMarker = document.querySelector("#animated-marker");
+        const aEntity = document.querySelector("#animated-model");
+
+        // every click, we make our model grow in size :)
+        animatedMarker.addEventListener('click', function(ev, target){
+           alert("salam, Şükür alındı.")
+        });
+}});
